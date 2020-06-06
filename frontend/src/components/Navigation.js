ee90c1c77eb9b4fae8cd5c2541113b6e755a44d7
Georg Preuss <preussg@gmail.com>
2020-06-06 13:10:33 +0100

    appbar changes

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,7 +1,7 @@
 import React, { useState, useContext, useEffect } from 'react'
 import { Link, useHistory } from 'react-router-dom'
 import clsx from 'clsx'
-import { makeStyles, useTheme } from '@material-ui/core/styles'
+import { fade, makeStyles, useTheme } from '@material-ui/core/styles'
 import Drawer from '@material-ui/core/Drawer'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import AppBar from '@material-ui/core/AppBar'
@@ -16,8 +16,21 @@ import ChevronRightIcon from '@material-ui/icons/ChevronRight'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemIcon from '@material-ui/core/ListItemIcon'
 import ListItemText from '@material-ui/core/ListItemText'
-import InboxIcon from '@material-ui/icons/MoveToInbox'
+import InputBase from '@material-ui/core/InputBase'
+import LibraryBooksIcon from '@material-ui/icons/LibraryBooks'
+import LocalLibraryIcon from '@material-ui/icons/LocalLibrary'
+import AccountBoxIcon from '@material-ui/icons/AccountBox'
+import LibraryAddIcon from '@material-ui/icons/LibraryAdd'
+import LocalGroceryStoreIcon from '@material-ui/icons/LocalGroceryStore'
+import SearchIcon from '@material-ui/icons/Search'
+import ExitToAppIcon from '@material-ui/icons/ExitToApp'
+import Badge from '@material-ui/core/Badge'
 import MailIcon from '@material-ui/icons/Mail'
+import NotificationsIcon from '@material-ui/icons/Notifications'
+import AccountCircle from '@material-ui/icons/AccountCircle'
+import MenuItem from '@material-ui/core/MenuItem'
+import Menu from '@material-ui/core/Menu'
+import MoreIcon from '@material-ui/icons/MoreVert'
 
 import { ModalContext } from './ModalContext'
 import SignupLoginModal from './SignupLoginModal'
@@ -28,6 +41,9 @@ const drawerWidth = 240
 const useStyles = makeStyles((theme) => ({
 	root: {
 		display: 'flex'
+	},
+	grow: {
+		flexGrow: 1
 	},
 	appBar: {
 		transition: theme.transitions.create(['margin', 'width'], {
@@ -79,6 +95,55 @@ const useStyles = makeStyles((theme) => ({
 			duration: theme.transitions.duration.enteringScreen
 		}),
 		marginLeft: 0
+	},
+	search: {
+		position: 'relative',
+		borderRadius: theme.shape.borderRadius,
+		backgroundColor: fade(theme.palette.common.white, 0.15),
+		'&:hover': {
+			backgroundColor: fade(theme.palette.common.white, 0.25)
+		},
+		marginRight: theme.spacing(2),
+		marginLeft: 0,
+		width: '100%',
+		[theme.breakpoints.up('sm')]: {
+			marginLeft: theme.spacing(3),
+			width: 'auto'
+		}
+	},
+	searchIcon: {
+		padding: theme.spacing(0, 2),
+		height: '100%',
+		position: 'absolute',
+		pointerEvents: 'none',
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	inputRoot: {
+		color: 'inherit'
+	},
+	inputInput: {
+		padding: theme.spacing(1, 1, 1, 0),
+		// vertical padding + font size from searchIcon
+		paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+		transition: theme.transitions.create('width'),
+		width: '100%',
+		[theme.breakpoints.up('md')]: {
+			width: '20ch'
+		}
+	},
+	sectionDesktop: {
+		display: 'none',
+		[theme.breakpoints.up('md')]: {
+			display: 'flex'
+		}
+	},
+	sectionMobile: {
+		display: 'flex',
+		[theme.breakpoints.up('md')]: {
+			display: 'none'
+		}
 	}
 }))
 
@@ -88,7 +153,7 @@ const Navigation = ({ children }) => {
 	const history = useHistory()
 	const [openDrawer, setOpenDrawer] = useState(true)
 	const { toggleModal } = useContext(ModalContext)
-	const [loggedIn, setLoggedIn] = useState(true) // not using loggedIn at the moment...
+	const [loggedIn, setLoggedIn] = useState(true) // not using loggedIn at the moment... move into useContext as this code won't allow multiple login logouts
 
 	const toggleDrawer = () => {
 		setOpenDrawer(!openDrawer)
@@ -99,6 +164,91 @@ const Navigation = ({ children }) => {
 		setLoggedIn(false) // bit of a cheat to get 'Signup / Login' to reappear
 		history.push('/')
 	}
+
+	const [anchorEl, setAnchorEl] = React.useState(null)
+	const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null)
+
+	const isMenuOpen = Boolean(anchorEl)
+
+	useEffect(() => {
+		console.log('oi: ', anchorEl)
+	}, [anchorEl])
+
+	const isMobileMenuOpen = Boolean(mobileMoreAnchorEl)
+
+	const handleProfileMenuOpen = (event) => {
+		setAnchorEl(event.currentTarget)
+	}
+
+	const handleMobileMenuClose = () => {
+		setMobileMoreAnchorEl(null)
+	}
+
+	const handleMenuClose = () => {
+		setAnchorEl(null)
+		handleMobileMenuClose()
+	}
+
+	const handleMobileMenuOpen = (event) => {
+		setMobileMoreAnchorEl(event.currentTarget)
+	}
+
+	const menuId = 'primary-search-account-menu'
+	const renderMenu = (
+		<Menu
+			anchorEl={anchorEl}
+			anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+			id={menuId}
+			keepMounted
+			transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+			open={isMenuOpen}
+			onClose={handleMenuClose}
+		>
+			<MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+			<MenuItem onClick={handleMenuClose}>My account</MenuItem>
+		</Menu>
+	)
+
+	const mobileMenuId = 'primary-search-account-menu-mobile'
+	const renderMobileMenu = (
+		<Menu
+			anchorEl={mobileMoreAnchorEl}
+			anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+			id={mobileMenuId}
+			keepMounted
+			transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+			open={isMobileMenuOpen}
+			onClose={handleMobileMenuClose}
+		>
+			<MenuItem>
+				<IconButton aria-label="show 4 new mails" color="inherit">
+					<Badge badgeContent={4} color="secondary">
+						<MailIcon />
+					</Badge>
+				</IconButton>
+				<p>Messages</p>
+			</MenuItem>
+			<MenuItem>
+				<IconButton aria-label="show 11 new notifications" color="inherit">
+					<Badge badgeContent={11} color="secondary">
+						<NotificationsIcon />
+					</Badge>
+				</IconButton>
+				<p>Notifications</p>
+			</MenuItem>
+			<MenuItem onClick={handleProfileMenuOpen}>
+				<IconButton
+					aria-label="account of current user"
+					aria-controls="primary-search-account-menu"
+					aria-haspopup="true"
+					color="inherit"
+				>
+					<AccountCircle />
+				</IconButton>
+				<p>Profile</p>
+			</MenuItem>
+		</Menu>
+	)
 
 	return (
 		<>
@@ -124,8 +274,60 @@ const Navigation = ({ children }) => {
 						<Typography variant="h6" noWrap>
 							MEAL-PLANNER
 						</Typography>
+						<div className={classes.search}>
+							<div className={classes.searchIcon}>
+								<SearchIcon />
+							</div>
+							<InputBase
+								placeholder="Search…"
+								classes={{
+									root: classes.inputRoot,
+									input: classes.inputInput
+								}}
+								inputProps={{ 'aria-label': 'search' }}
+							/>
+						</div>
+						<div className={classes.grow} />
+						<div className={classes.sectionDesktop}>
+							<IconButton aria-label="show 4 new mails" color="inherit">
+								<Badge badgeContent={4} color="secondary">
+									<MailIcon />
+								</Badge>
+							</IconButton>
+							<IconButton
+								aria-label="show 17 new notifications"
+								color="inherit"
+							>
+								<Badge badgeContent={17} color="secondary">
+									<NotificationsIcon />
+								</Badge>
+							</IconButton>
+							<IconButton
+								edge="end"
+								aria-label="account of current user"
+								aria-controls={menuId}
+								aria-haspopup="true"
+								onClick={handleProfileMenuOpen}
+								color="inherit"
+							>
+								<AccountCircle />
+							</IconButton>
+						</div>
+						<div className={classes.sectionMobile}>
+							<IconButton
+								aria-label="show more"
+								aria-controls={mobileMenuId}
+								aria-haspopup="true"
+								onClick={handleMobileMenuOpen}
+								color="inherit"
+							>
+								<MoreIcon />
+							</IconButton>
+						</div>
 					</Toolbar>
 				</AppBar>
+				{renderMobileMenu}
+				{renderMenu}
 				<Drawer
 					className={classes.drawer}
 					variant="persistent"
@@ -146,20 +348,15 @@ const Navigation = ({ children }) => {
 					</div>
 					<Divider />
 					<List>
-						{/* {!Auth.isAuthorized() && (
-							<ListItem button onClick={toggleModal}>
-								<ListItemText>Signup / Login</ListItemText>
-							</ListItem>
-						)}
-						{Auth.isAuthorized() && (
-							<ListItem button onClick={signout}>
-								<ListItemText>Signout</ListItemText>
-							</ListItem>
-						)} */}
 						<ListItem
 							button
 							onClick={!Auth.isAuthorized() ? toggleModal : signout}
 						>
+							{Auth.isAuthorized() && (
+								<ListItemIcon>
+									<ExitToAppIcon />
+								</ListItemIcon>
+							)}
 							<ListItemText>
 								{!Auth.isAuthorized() ? 'Signup / Login' : 'Signout'}
 							</ListItemText>
@@ -168,23 +365,47 @@ const Navigation = ({ children }) => {
 					<Divider />
 					<List>
 						<ListItem button component={Link} to="/">
+							<ListItemIcon>
+								<LibraryBooksIcon />
+							</ListItemIcon>
 							<ListItemText>Browse recipes</ListItemText>
 						</ListItem>
-						<ListItem>
-							<ListItemText>Recipe collection</ListItemText>
+						<ListItem button>
+							<ListItemIcon>
+								<LocalLibraryIcon />
+							</ListItemIcon>
+							<ListItemText>Recipe collections</ListItemText>
 						</ListItem>
-						<ListItem>
-							<ListItemText>My creations</ListItemText>
+						<ListItem button>
+							<ListItemIcon>
+								<LibraryAddIcon />
+							</ListItemIcon>
+							<ListItemText>New recipe</ListItemText>
 						</ListItem>
-						{['All mail', 'Trash', 'Spam'].map((text, index) => (
+						<ListItem button>
+							<ListItemIcon>
+								<LocalGroceryStoreIcon />
+							</ListItemIcon>
+							<ListItemText>New shopping list</ListItemText>
+						</ListItem>
+						<ListItem button>
+							<ListItemIcon>
+								<SearchIcon />
+							</ListItemIcon>
+							<ListItemText>Search recipes</ListItemText>
+						</ListItem>
+						{/* {['All mail', 'Trash', 'Spam'].map((text, index) => (
 							<ListItem button key={text}>
 								<ListItemIcon>
 									{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
 								</ListItemIcon>
 								<ListItemText primary={text} />
 							</ListItem>
-						))}
+						))} */}
 						<ListItem button component={Link} to="/account">
+							<ListItemIcon>
+								<AccountBoxIcon />
+							</ListItemIcon>
 							<ListItemText>Account Settings</ListItemText>
 						</ListItem>
 					</List>
